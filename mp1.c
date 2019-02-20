a5f00d07cc3af80c3047171d3855f7a699b9f173
LegendCHN <ningkaiwu@gmail.com>
2019-02-20 04:52:35 -0600

    proc

--- mp1.c
+++ mp1.c
@@ -8,7 +8,34 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Group_ID");
 MODULE_DESCRIPTION("CS-423 MP1");
 
+#define FILENAME "status"
+#define DIRECTORY "mp1"
 #define DEBUG 1
+
+static const struct file_operations mp1_file = {
+.owner = THIS_MODULE,
+.read = mp1_read,
+.write = mp1_write,
+};
+static struct proc_dir_entry *proc_dir;
+static struct proc_dir_entry *proc_entry;
+
+static ssize_t mp1_read (struct file *file, char __user *buffer, size_t count, loff_t *data){
+   // implementation goes here...
+   int copied;
+   char * buf;
+   buf = (char *) kmalloc(count,GFP_KERNEL);
+   copied = 0;
+   //… put something into the buf, updated copied
+   copy_to_user(buffer, buf, copied);
+   kfree(buf);
+   return copied ;
+}
+
+static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t count, loff_t *data){
+   // implementation goes here...
+   return 0;
+}
 
 // mp1_init - Called when module is loaded
 int __init mp1_init(void)
@@ -17,8 +44,8 @@ int __init mp1_init(void)
    printk(KERN_ALERT "MP1 MODULE LOADING\n");
    #endif
    // Insert your code here ...   
-   
-   
+   proc_dir = proc_mkdir(DIRECTORY, NULL);
+   proc_entry = proc_create(FILENAME, 0666, proc_dir, & mp1_file);
    printk(KERN_ALERT "MP1 MODULE LOADED\n");
    return 0;   
 }
@@ -29,6 +56,8 @@ void __exit mp1_exit(void)
    #ifdef DEBUG
    printk(KERN_ALERT "MP1 MODULE UNLOADING\n");
    #endif
+   remove_proc_entry(FILENAME, proc_dir);
+   remove_proc_entry(DIRECTORY, NULL);  
    // Insert your code here ...
    
    
@@ -39,4 +68,3 @@ void __exit mp1_exit(void)
 // Register init and exit funtions
 module_init(mp1_init);
 module_exit(mp1_exit);
-MODULE_LICENSE("GPL");
