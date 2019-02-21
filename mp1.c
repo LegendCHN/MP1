db8d413a9ef174c012b00198be799a82eb1b0e4b
LegendCHN <ningkaiwu@gmail.com>
2019-02-20 18:00:49 -0600

    read&write

--- mp1.c
+++ mp1.c
@@ -20,6 +20,7 @@ MODULE_DESCRIPTION("CS-423 MP1");
 #define FILENAME "status"
 #define DIRECTORY "mp1"
 #define DEBUG 1
+#define MASK 0666
 
 static const struct file_operations mp1_file = {
 .owner = THIS_MODULE,
@@ -29,22 +30,51 @@ static const struct file_operations mp1_file = {
 
 static struct proc_dir_entry *proc_dir;
 static struct proc_dir_entry *proc_entry;
+struct linkedlist reglist;
+struct linkedlist *tmp;
+struct list_head *pos, *q;
+static pthread_mutex_t lock;
+
+void free_linkedlist(){
+   list_for_each_safe(pos, q, &mylist.list){
+       tmp= list_entry(pos, struct linkedlist, list);
+       list_del(pos);
+       free(tmp);
+   }
+}
 
 static ssize_t mp1_read (struct file *file, char __user *buffer, size_t count, loff_t *data){
-   // implementation goes here...
    int copied;
-   char * buf;
-   buf = (char *) kmalloc(count,GFP_KERNEL);
    copied = 0;
-   //… put something into the buf, updated copied
+   int  length;
+   char * buf;
+   buf = (char *) kmalloc(count, GPL);
+
+   if(*data > 0){
+      kfree(buf);
+      return 0;
+   }
+
+   list_for_each_entry(tmp, &reglist.list, list){
+      length = sprintf(buf + copied, "PID: %d, TIME: %lu\n", tmp->pid, tmp->time);
+      copied += length;
+   }
    copy_to_user(buffer, buf, copied);
    kfree(buf);
+
+   *data += copied;
    return copied ;
 }
 
 static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t count, loff_t *data){
-   // implementation goes here...
-   return 0;
+   tmp = (pid_time_list*)kmalloc(sizeof(struct pid_time_List), GPL);
+   char buf[100];
+   copy_from_user(buf, buffer, count);
+   sscanf(buf, "%d", &tmp->pid);
+   pthread_mutex_lock(&lock);
+   list_add(&(tmp->list), &(reglist.list));
+   pthread_mutex_unlock(&lock);
+   return count;
 }
 
 // mp1_init - Called when module is loaded
@@ -55,7 +85,10 @@ int __init mp1_init(void)
    #endif
    // Insert your code here ...   
    proc_dir = proc_mkdir(DIRECTORY, NULL);
-   proc_entry = proc_create(FILENAME, 0666, proc_dir, & mp1_file);
+   proc_entry = proc_create(FILENAME, MASK, proc_dir, & mp1_file);
+   INIT_LIST_HEAD(&reglist.list);
+
+   pthread_mutex_init(&lock, NULL);
    printk(KERN_ALERT "MP1 MODULE LOADED\n");
    return 0;   
 }
@@ -67,11 +100,9 @@ void __exit mp1_exit(void)
    printk(KERN_ALERT "MP1 MODULE UNLOADING\n");
    #endif
    remove_proc_entry(FILENAME, proc_dir);
-   remove_proc_entry(DIRECTORY, NULL);  
-   // Insert your code here ...
-   
-   
-
+   remove_proc_entry(DIRECTORY, NULL);
+   free_linkedlist();
+   pthread_mutex_destroy(&lock);
    printk(KERN_ALERT "MP1 MODULE UNLOADED\n");
 }
 
--- mp1_given.h
+++ mp1_given.h
@@ -6,6 +6,12 @@
 
 #define find_task_by_pid(nr) pid_task(find_vpid(nr), PIDTYPE_PID)
 
+struct linkedlist{
+  struct list_head list;
+   int pid;
+   unsigned long time;
+};
+
 //THIS FUNCTION RETURNS 0 IF THE PID IS VALID AND THE CPU TIME IS SUCCESFULLY RETURNED BY THE PARAMETER CPU_USE. OTHERWISE IT RETURNS -1
 int get_cpu_use(int pid, unsigned long *cpu_use)
 {
@@ -26,9 +32,9 @@ int get_cpu_use(int pid, unsigned long *cpu_use)
 }
 
 //
-static ssize_t mp1_read (struct file *file, char __user *buffer, size_t count, loff_t *data);
-//
-static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t count, loff_t *data);
+// static ssize_t mp1_read (struct file *file, char __user *buffer, size_t count, loff_t *data);
+// //
+// static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t count, loff_t *data);
 
-
+// void free_linkedlist();
 #endif
