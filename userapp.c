47adecb8cb65a2b4534919d5104f3fdcdcad5967
LegendCHN <ningkaiwu@gmail.com>
2019-02-20 18:43:13 -0600

    userapp

--- userapp.c
+++ userapp.c
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
 	int pid;
 	pid = getpid();
     char cmd[100];
-    sprintf(cmd, "echo %u > /proc/mp1/status", pid);
+    sprintf(cmd, "echo %d > /proc/mp1/status", pid);
     system(command);
 	return 0;
 }
